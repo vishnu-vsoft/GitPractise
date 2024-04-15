ed7d9df805cf83353c18c3bdeec059b66adb355f
vishnu-vsoft <vishnu.sudarsan@virtuosoft.in>
2024-04-15 12:39:03 +0530

    aded if condition

--- GitPractise/Program.cs
+++ GitPractise/Program.cs
@@ -13,12 +13,22 @@ namespace GitPractise
             int i = 0;
             while (i!=5)
             {   
-                Console.WriteLine("Enter your option 1) Conversion\n 5)Exit");
+                Console.WriteLine("Enter your option 1) Conversion in INR\n2)Conversion to AUS Dollar\n 5)Exit");
                 i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Enter American dollars to be converted into Indian rupees");
-                double convertedAmount = Convert.ToDouble(Console.ReadLine()) * 84.44;
-               
-                Console.WriteLine($"Converted amount is {convertedAmount}");
+                if (i == 1)
+                {
+                    double convertedAmount = Convert.ToDouble(Console.ReadLine()) * 84.44;
+                    Console.WriteLine($"Converted amount is {convertedAmount}");
+                }
+                if (i == 2)
+                {
+                    double ausDollar = Convert.ToDouble(Console.ReadLine()) * 1.54;
+                    Console.WriteLine($"Converted amount is {ausDollar}");
+                }
+                
+                
+                
                 Console.ReadLine();
             }
             
