f5d96f9301d1da202a1d7e1570494d7129cd629f
vishnu-vsoft <vishnu.sudarsan@virtuosoft.in>
2024-04-15 10:43:34 +0530

    Add project files.

---
+++ GitPractise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter American dollars to be converted into Indian rupees");
+            string amount = Console.ReadLine();
+            double convertedAmount = double.Parse(amount) * 83.44;
+            
+            Console.WriteLine($"Converted amount is {convertedAmount}");
+            Console.ReadLine();
+        }
+    }
+}
+        
